bbcff855590d32e22a2ad7a6ec35cd8180f36740
JABBA\msi <jb@appstab.co>
2020-07-21 14:51:55 +0800

    Playing with cookie parser

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,41 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import './app.css';
-import ReactImage from './react.png';
+// import ReactImage from './react.png';
 
-export default class App extends Component {
-  state = { username: null };
+const App = () => {
+  const [state, setState] = useState({ username: null });
+  const [players, setPlayers] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     fetch('/api/getUsername')
       .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+      .then(user => setState({ username: user.username }));
+  }, [])
+
+  const onGetToken = () => {
+    fetch('/api/getToken')
+      .then(res => res.json())
+      .then(res => console.log(res));
   }
 
-  render() {
-    const { username } = this.state;
-    return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-      </div>
-    );
+  const getTriviaPoints = () => {
+    fetch('/api/triviaPoints')
+      .then(res => res.json())
+      .then(res => setPlayers(res));
   }
+
+  const { username } = state;
+  return (
+    <div>
+      {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+      <button type="button" onClick={onGetToken}>Get Token!</button>
+      <button type="button" onClick={getTriviaPoints}> Get Trivia Ladder</button>
+
+      <ul>
+        {players.map(p => <li>{p.name} - {p.score}</li>)}
+      </ul>
+    </div>
+  );
 }
+
+export default App
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const os = require('os');
+const cookieParser = require('cookie-parser')
+
 
 const app = express();
 
+const accessToken = "ABCDEFGH";
+
+app.use(cookieParser());
+
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.get('/api/getToken', (req, res) => {
+
+    // Set a cookie
+    res.cookie('myCookie', accessToken, {
+        maxAge: 60 * 60 * 1000, // 1 hour
+        httpOnly: true,
+        // secure: true,
+        sameSite: true,
+    })
+
+    res.send({ isSuccess: true });
+
+})
+
+app.get('/api/triviaPoints', (req, res) => {
+    console.log("req cookie", req.cookies);
+    if (req.cookies && req.cookies.myCookie === accessToken) {
+        res.send([{ name: 'Ang', score: 17 }, { name: 'Olly', score: 16 }])
+    } else {
+        res.status(401).send('ooops')
+    }
+})
+
+
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
